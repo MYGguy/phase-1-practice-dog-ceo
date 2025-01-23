65fd4b571163398fa44d6da6c50be6de042b1526
Moshe Goldstein <mygguy613@gmail.com>
2025-01-23 18:02:42 -0500

    add just one image to dom

--- src/index.js
+++ src/index.js
@@ -3,4 +3,7 @@ console.log('%c HI', 'color: firebrick')
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
 const breedUrl = "https://dog.ceo/api/breeds/list/all";
 
-
+fetch(imgUrl)
+    .then(res => res.json())
+    // .then(json => console.log(json))
+    .then(json => document.querySelector('#dog-image-container').innerHTML = `<img src='${json.message[0]}'</img>`);
