e1e58f9d6be852b507f8fdaade05304e07276b27
Moshe Goldstein <mygguy613@gmail.com>
2025-01-23 18:30:38 -0500

    add all 4 images to dom

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-console.log('%c HI', 'color: firebrick')
+console.log('%c HI', 'color: green')
 
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
 const breedUrl = "https://dog.ceo/api/breeds/list/all";
@@ -6,4 +6,13 @@ const breedUrl = "https://dog.ceo/api/breeds/list/all";
 fetch(imgUrl)
     .then(res => res.json())
     // .then(json => console.log(json))
-    .then(json => document.querySelector('#dog-image-container').innerHTML = `<img src='${json.message[0]}'</img>`);
+    .then(json => handleImages(json));
+
+function handleImages(json) {
+    json.message.forEach(image => {
+        const img = document.createElement('img');
+        img.src = image;
+        img.style.width = '200px';
+        document.querySelector('#dog-image-container').appendChild(img);
+    })
+}
